825a2f56aababa29719e2616de2f602f430cca92
lupoAI <salomonlupo@gmail.com>
2021-07-05 18:04:18 +0100

    started new branch to experiment with fcn rules

--- analysis/market_analyzer.py
+++ analysis/market_analyzer.py
@@ -131,8 +131,7 @@ class SimulatedMarketAnalyzer:
         return stylized_facts
 
 
-class MarketVisualizer:
-
+class MarketVisualizerAbstract:
     def __init__(self, market_prices, is_simulated=False):
         self.market_prices = market_prices
         self.is_simulated = is_simulated
@@ -141,6 +140,12 @@ class MarketVisualizer:
             self.market_prices = self.market_prices['Close']
         else:
             self.market_analyzer = SimulatedMarketAnalyzer(self.market_prices)
+
+
+class MarketVisualizer(MarketVisualizerAbstract):
+
+    def __init__(self, market_prices, is_simulated=False):
+        super().__init__(market_prices, is_simulated)
 
     def visualize_market(self, returns_interval: Union[int, str], save_name: Union[str, None] = None):
         if returns_interval == '1d' and not self.is_simulated:
@@ -187,9 +192,68 @@ class MarketVisualizer:
             plt.savefig(save_name)
         plt.show()
 
+    def compare_market(self, returns_interval: Union[int, str], other: MarketVisualizerAbstract,
+                       save_name: Union[str, None] = None):
+        if returns_interval == '1d' and not self.is_simulated:
+            stylized_facts = self.market_analyzer.get_daily_market_metrics()
+        elif returns_interval == '1d' and self.is_simulated:
+            raise ValueError('1d is only available for real market data')
+        else:
+            stylized_facts = self.market_analyzer.get_market_metrics(returns_interval)
+        if returns_interval == '1d' and not other.is_simulated:
+            other_stylized_facts = other.market_analyzer.get_daily_market_metrics()
+        elif returns_interval == '1d' and other.is_simulated:
+            raise ValueError('1d is only available for real market data')
+        else:
+            other_stylized_facts = other.market_analyzer.get_market_metrics(returns_interval)
+
+        ax1 = plt.subplot2grid(shape=(3, 6), loc=(0, 0), colspan=6)
+        ax1_1 = ax1.twiny()
+        ax2 = plt.subplot2grid((3, 6), (1, 0), colspan=2)
+        ax3 = plt.subplot2grid((3, 6), (1, 2), colspan=2)
+        ax4 = plt.subplot2grid((3, 6), (1, 4), colspan=2)
+        ax5 = plt.subplot2grid((3, 6), (2, 0), colspan=6)
+        scaled_market_prices = self.market_prices / self.market_prices[0]
+        scaled_other_market_prices = other.market_prices / other.market_prices[0]
+        ax1.plot(scaled_market_prices, color='green', label='Price 1')
+        ax1_1.plot(scaled_other_market_prices, color='yellow', label='Price 2')
+        ax1.grid(True)
+        ax1.legend(fontsize=8)
+        ax1_1.legend(fontsize=8)
+        ax2.plot(stylized_facts.auto_correlation, color='red', label='1')
+        ax2.plot(other_stylized_facts.auto_correlation, color='blue', label='2')
+        ax2.grid(True)
+        ax2.set_xticks(list(range(1, 11)))
+        ax2.set_title('Auto-Correlation', size=10)
+        ax2.legend(fontsize=8)
+        ax3.plot(stylized_facts.volatility_clustering, color='orange', label='1')
+        ax3.plot(other_stylized_facts.volatility_clustering, color='green', label='2')
+        ax3.grid(True)
+        ax3.set_xticks(list(range(1, 11)))
+        ax3.set_title('Volatility Clustering', size=10)
+        ax3.legend(fontsize=8)
+        ax4.plot(stylized_facts.leverage_effect, color='purple', label='1')
+        ax4.plot(other_stylized_facts.leverage_effect, color='brown', label='2')
+        ax4.grid(True)
+        ax4.set_xticks(list(range(1, 11)))
+        ax4.set_title('Leverage Effect', size=10)
+        ax4.legend(fontsize=8)
+        bins = np.linspace(-50, 50, 1000)
+        centers = (bins[1:] + bins[:-1]) / 2
+        normal_distribution = normal_pdf(centers)
+        ax5.hist(stylized_facts.rets, bins=bins, density=True, label='1', alpha=0.5)
+        ax5.hist(other_stylized_facts.rets, bins=bins, density=True, label='2', alpha=0.5)
+        ax5.plot(centers, normal_distribution, label='Normal PDF')
+        ax5.set_xlim(-5, 5)
+        ax5.set_xticks(list(range(-5, 6)))
+        ax5.set_title('Return Distribution', size=10)
+        ax5.legend(fontsize=8)
+        plt.subplots_adjust(hspace=0.55, wspace=1)
+        plt.suptitle(f'Markets comparison for {returns_interval} periods returns')
+        if save_name is not None:
+            plt.savefig(save_name)
+        plt.show()
+
 
 def normal_pdf(values):
     return np.exp(-values ** 2 / 2) / np.sqrt(2 * np.pi)
-
-
-
--- mains/visualize_market.py
+++ mains/visualize_market.py
@@ -1,4 +1,6 @@
-from analysis.market_analyzer import  MarketVisualizer
+from analysis.market_analyzer import MarketVisualizer
+from market.simulator import RandomSimulator
+from market.exchange import Exchange
 import pandas as pd
 import pickle
 
@@ -7,17 +9,17 @@ data = pd.read_csv('../data/spx/SPX_1min.txt', header=None, index_col=0, parse_d
 data = data.drop(columns=[5])
 data.columns = headers
 
-market_visualizer = MarketVisualizer(data)
-market_visualizer.visualize_market(1, '../results/spx_1m_rets_market.jpg')
-features_1m = market_visualizer.market_analyzer.get_market_metrics(1)
-market_visualizer.visualize_market(5, '../results/spx_5m_rets_market.jpg')
-features_5m = market_visualizer.market_analyzer.get_market_metrics(5)
-market_visualizer.visualize_market(15, '../results/spx_15m_rets_market.jpg')
-features_15m = market_visualizer.market_analyzer.get_market_metrics(15)
-market_visualizer.visualize_market(30, '../results/spx_30m_rets_market.jpg')
-features_30m = market_visualizer.market_analyzer.get_market_metrics(30)
-market_visualizer.visualize_market('1d', '../results/spx_1d_rets_market.jpg')
-features_1d = market_visualizer.market_analyzer.get_daily_market_metrics()
+real_market_visualizer = MarketVisualizer(data)
+real_market_visualizer.visualize_market(1, '../results/spx_1m_rets_market.jpg')
+features_1m = real_market_visualizer.market_analyzer.get_market_metrics(1)
+real_market_visualizer.visualize_market(5, '../results/spx_5m_rets_market.jpg')
+features_5m = real_market_visualizer.market_analyzer.get_market_metrics(5)
+real_market_visualizer.visualize_market(15, '../results/spx_15m_rets_market.jpg')
+features_15m = real_market_visualizer.market_analyzer.get_market_metrics(15)
+real_market_visualizer.visualize_market(30, '../results/spx_30m_rets_market.jpg')
+features_30m = real_market_visualizer.market_analyzer.get_market_metrics(30)
+real_market_visualizer.visualize_market('1d', '../results/spx_1d_rets_market.jpg')
+features_1d = real_market_visualizer.market_analyzer.get_daily_market_metrics()
 
 with open('../data/spx_processed/features_1m.pickle', 'wb') as f_1m:
     pickle.dump(features_1m, f_1m)
@@ -29,3 +31,15 @@ with open('../data/spx_processed/features_30m.pickle', 'wb') as f_30m:
     pickle.dump(features_30m, f_30m)
 with open('../data/spx_processed/features_1d.pickle', 'wb') as f_1d:
     pickle.dump(features_1d, f_1d)
+
+
+exchange = Exchange()
+random_simulator = RandomSimulator(exchange, 100)
+random_simulator.run(5000, 20, 500, 5, 20)
+simulation_price = random_simulator.last_mid_price_series
+simulated_market_visualizer = MarketVisualizer(simulation_price.price, is_simulated=True)
+
+simulated_market_visualizer.visualize_market(1, '../results/random_simulation_1m_rets_market.jpg')
+simulated_market_visualizer.visualize_market(5, '../results/random_simulation_5m_rets_market.jpg')
+simulated_market_visualizer.visualize_market(15, '../results/random_simulation_15m_rets_market.jpg')
+simulated_market_visualizer.visualize_market(30, '../results/random_simulation_30m_rets_market.jpg')
